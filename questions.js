322f47dd38744b6771a8995470a9da6475661f35
Bushra04Shahzad <114739735+Bushra04Shahzad@users.noreply.github.com>
2022-11-04 22:18:19 +0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: "script",
+    options: [
+      "script",
+      "js",
+      "scripting",
+      "JavaScript"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is a server-side Java Script object?",
+    answer: "Function",
+    options: [
+      "Function",
+      "File",
+      "FileUpload",
+      "Date"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Java script can be used for Storing the form's contents to a database file on the server",
+    answer: "False",
+    options: [
+      "True",
+      "False",
+      "Maybe",
+      "Unknown"
+    ]
+  },
+    {
+    numb: 4,
+    question: "To insert a JavaScript into an HTML page, which tag is used?",
+    answer: "javascript",
+    options: [
+      " script=â€™javaâ€™",
+      "javascript",
+      "script",
+      "js"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the below is used in Java script to insert special characters?",
+    answer: "/",
+    options: [
+      "&",
+      "*",
+      "/",
+      "%"
+    ]
+  },
+
+];
